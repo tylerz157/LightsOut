6dfb17847db28b0d529e7b5f09bac1fb75afd742
tylerz157 <tzhang@eastsideprep.org>
2023-05-09 21:47:18 -0700

    Only showing relevant light rays
    Raycast to each vector, if there is something in the way, it is not relevant and dont draw it.
    
    One weird thing is that the raycast will intersect with the collider at the end point so that might be a little annoying to deal with later.

--- LightsOutProject/test_scene.gd
+++ LightsOutProject/test_scene.gd
@@ -2,6 +2,7 @@ extends Node2D
 
 var marker = preload("res://marker.tscn")
 var ground_verts = []
+var draw_verts = []
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -14,24 +15,52 @@ func _ready():
 		
 		# get vectors from center of obj to rectangle corners
 		var corners = []
-		corners.append(Vector2((rect.size[0] * gscale[0])/2 + gpos[0], (rect.size[1] * gscale[1])/2 + gpos[1]))
-		corners.append(Vector2(-(rect.size[0] * gscale[0])/2 + gpos[0], (rect.size[1] * gscale[1])/2 + gpos[1]))
-		corners.append(Vector2(-(rect.size[0] * gscale[0])/2 + gpos[0], -(rect.size[1] * gscale[1])/2 + gpos[1]))
-		corners.append(Vector2((rect.size[0] * gscale[0])/2 + gpos[0], -(rect.size[1] * gscale[1])/2 + gpos[1]))
+		corners.append(Vector2((rect.size[0] * gscale[0])/2, (rect.size[1] * gscale[1])/2))
+		corners.append(Vector2(-(rect.size[0] * gscale[0])/2, (rect.size[1] * gscale[1])/2))
+		corners.append(Vector2(-(rect.size[0] * gscale[0])/2, -(rect.size[1] * gscale[1])/2))
+		corners.append(Vector2((rect.size[0] * gscale[0])/2, -(rect.size[1] * gscale[1])/2))
 		
-		for i in range(4):
-			var mark = marker.instantiate()
-			mark.set_position(Vector2(corners[i-1][0], corners[i-1][1]))
-			add_child(mark)
+		# set markers on the corners to show it is the right spot
+		#for i in range(4):
+		#	var mark = marker.instantiate()
+		#	mark.set_position(Vector2(corners[i-1][0] + gpos[0], corners[i-1][1] + gpos[1]))
+		#	add_child(mark)
 		
-		ground_verts.append(corners)
+		# add to global var to access during updates
+		ground_verts.append([ground, corners])
 	print(ground_verts)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	queue_redraw()
+
+func _physics_process(delta):
+	# clear draw
+	draw_verts = []
 	
+	# select which light rays are actually important
+	for object in ground_verts:
+		
+		var space_state = get_world_2d().direct_space_state
+		# make corner to vectors to corners
+		var gpos = object[0].position
+		for corner in object[1]:
+			var vect = Vector2(corner[0] + gpos[0], corner[1] + gpos[1])
+			var query = PhysicsRayQueryParameters2D.create($Player.position, vect)
+			query.exclude = [$Player]
+			var result = space_state.intersect_ray(query)
+			if result == {}:
+				draw_verts.append(vect)
+			elif result != null:
+				if (result.position - vect).length() <= 1:
+					draw_verts.append(vect)
+
+# draw relevant light rays
 func _draw():
-	for corners in ground_verts:
-		for corner in corners:	
-			draw_line($Player.position, corner, Color(1,1,1))
+	for vertex in draw_verts:	
+		draw_line($Player.position, vertex, Color(1,1,1))
+			
+			
+			
+# shape of desired database:
+# object: vertices related to center, vectors that form edges
