e9855c6819222d3107291d43dbfcc60eb602ac57
tylerz157 <tzhang@eastsideprep.org>
2023-05-09 23:11:59 -0700

    Found only relevant light rays
    I further found relevant light rays by determining if the light ray would hit a solid surface.
    To do this, I took the angle bisectors between the two edges and the light ray, and took the dot product between them to get the angle. If the angle is greater than 45 degrees, then it hits a solid wall and is not a relevant light ray.
    
    Iterations:
    - Tried checking if the dot products between the two edges and the light ray were both under 180 degrees, which would mean that it is solid. Didn't work because dot product doesn't give angles above 180.
    - Struggled with lines flashing in and out, was because of floating point error comparing to a constant pi/4
    
    Misc:
    - Removed gravity for the player to make it easier to test
    - Showed collision boxes and removed icon for better visibility
    - Made some testing drawings to visualize what was going on

--- LightsOutProject/test_scene.gd
+++ LightsOutProject/test_scene.gd
@@ -4,6 +4,9 @@ var marker = preload("res://marker.tscn")
 var ground_verts = []
 var draw_verts = []
 
+var test_draw = []
+var test_red = []
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	
@@ -37,6 +40,8 @@ func _process(delta):
 func _physics_process(delta):
 	# clear draw
 	draw_verts = []
+	test_draw = []
+	test_red = []
 	
 	# select which light rays are actually important
 	for object in ground_verts:
@@ -44,21 +49,57 @@ func _physics_process(delta):
 		var space_state = get_world_2d().direct_space_state
 		# make corner to vectors to corners
 		var gpos = object[0].position
-		for corner in object[1]:
-			var vect = Vector2(corner[0] + gpos[0], corner[1] + gpos[1])
+		for i in range(len(object[1])):
+			var corners = object[1]
+			var vect = Vector2(corners[i-1][0] + gpos[0], corners[i-1][1] + gpos[1])
+			var edge1 = corners[i%len(object[1])] - corners[i-1]
+			var edge2 = corners[(i-2)%len(object[1])] - corners[i-1]
+			
+			var light_ray = vect - $Player.position
 			var query = PhysicsRayQueryParameters2D.create($Player.position, vect)
 			query.exclude = [$Player]
 			var result = space_state.intersect_ray(query)
+			
+			# check if it intersects with anything in front
+			var blocked = true
 			if result == {}:
-				draw_verts.append(vect)
+				blocked = false
 			elif result != null:
 				if (result.position - vect).length() <= 1:
-					draw_verts.append(vect)
+					blocked = false
+			
+			# check if the light is colliding or keeps going afterwards
+			var solid = false
+			var betweenv1 = (edge1.normalized() - light_ray.normalized()).normalized()
+			var betweenv2 = (edge2.normalized() - light_ray.normalized()).normalized()
+			var angle = acos(betweenv1.dot(betweenv2))
+			
+			
+			
+			# account for floating point error
+			if(angle - 0.001 >= PI/4):
+				solid = true
+				test_red.append([corners[i-1]+gpos, betweenv1*20])
+				test_red.append([corners[i-1]+gpos, betweenv2*20])
+			else:
+				test_draw.append([corners[i-1]+gpos, betweenv1*20])
+				test_draw.append([corners[i-1]+gpos, betweenv2*20])
+							
+			# draw it if its good
+			if blocked == false and solid == false:
+				draw_verts.append(vect)
 
 # draw relevant light rays
 func _draw():
 	for vertex in draw_verts:	
-		draw_line($Player.position, vertex, Color(1,1,1))
+		draw_line($Player.position, vertex, Color(1,1,1), 1)
+	for pair in test_draw:
+		draw_line(pair[0], pair[1] + pair[0], Color.LIGHT_GREEN, 1)
+		draw_circle(pair[1] + pair[0], 1, Color.LIGHT_GREEN)
+	for pair in test_red:
+		draw_line(pair[0], pair[1] + pair[0], Color.RED, 1)
+		draw_circle(pair[1] + pair[0], 1, Color.RED)
+	
 			
 			
 			
