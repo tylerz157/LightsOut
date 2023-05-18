6f9d1ad85193c96757e39177522155476ab9ebd9
tylerz157 <tzhang@eastsideprep.org>
2023-05-17 22:18:01 -0700

    Added player animation
    Restarting adding art, first with player
    
    no bug yet

--- LightsOutProject/player.gd
+++ LightsOutProject/player.gd
@@ -4,6 +4,7 @@ enum State {FREE, HOLDING}
 var held_obj = null
 const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
+var is_jump = false
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
@@ -17,6 +18,8 @@ func _physics_process(delta):
 	# Handle Jump.
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
+		$AnimatedSprite2D.play("jump")
+		is_jump = true
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
@@ -25,8 +28,22 @@ func _physics_process(delta):
 		velocity.x = direction * SPEED
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)	
+		
+	if(is_jump == true):
+		pass
+	elif(velocity == Vector2(0,0)):
+		$AnimatedSprite2D.play("idle")
+	elif(velocity.x > 0):
+		$AnimatedSprite2D.play("move_right")
+		$AnimatedSprite2D.flip_h = false
+	elif(velocity.x < 0):
+		$AnimatedSprite2D.play("move_right")
+		$AnimatedSprite2D.flip_h = true
 
 	move_and_slide()
+	
+	if(is_on_floor()):
+		is_jump = false
 	
 	# grab object
 	if Input.is_action_just_pressed("grab"):
