b0de9b494c1528fffd4d091dc0425dc3f38f47c2
tylerz157 <tzhang@eastsideprep.org>
2023-05-22 09:33:46 -0700

    Working on collision polygons
    replacing collision lines for the light rays with full polygons for less bugs, still working on making the actual collision, but the area is drawn out

--- LightsOutProject/light_manager.gd
+++ LightsOutProject/light_manager.gd
@@ -1,40 +1,54 @@
 extends Node2D
 
-var marker = preload("res://marker.tscn")
 var light_wall = preload("res://light_wall.tscn")
-var key = preload("res://key.tscn")
-var ray_exclude = []
+var excluded_obj = []
 var lantern
-var ground_verts = []
-var draw_verts = []
+var shadowcast_obj = []
 
+var draw_verts = []
 var test_draw = []
 var test_red = []
+var test_points = []
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	
-	lantern = get_parent().find_children("Lantern")[0]
-	ray_exclude.append(get_parent().find_children("Player")[0])
-	ray_exclude.append(lantern)
-	ray_exclude.append(key)
-	
-	# find all the walls in level and get vertices
-	for ground in get_parent().find_children("Ground*"):
-		var gpos = ground.position
-		var gscale = ground.scale
-		var rect = ground.get_node("CollisionShape2D").shape.get_rect()
+	# setup:
+	print(get_parent().get_children())
+	# loop through every object and determine if it casts light rays.
+	for object in get_parent().get_children():
+		# if ground, add to list, if border, dont, neither, add to raycast ignore list
+		print(object.get_name())
+		if(object.get_name().contains("Ground")):
+			print('found ground')
+			var opos = object.position
+			var oscale = object.scale
+			var rect = object.get_node("CollisionShape2D").shape.get_rect()
+			
+			# get vectors for vertex positions
+			var vertices = []
+			vertices.append(Vector2(opos.x + (rect.size[0] * oscale[0])/2, opos.y + (rect.size[1] * oscale[1])/2))
+			vertices.append(Vector2(opos.x - (rect.size[0] * oscale[0])/2, opos.y + (rect.size[1] * oscale[1])/2))
+			vertices.append(Vector2(opos.x - (rect.size[0] * oscale[0])/2, opos.y - (rect.size[1] * oscale[1])/2))
+			vertices.append(Vector2(opos.x + (rect.size[0] * oscale[0])/2, opos.y - (rect.size[1] * oscale[1])/2))
+			
+			# set up database
+			var collider = light_wall.instantiate()
+			collider.collision_layer = 32
+			collider.collision_mask = 4
+			shadowcast_obj.append([object, vertices, collider])
+			
+		# if border, do nothing
+		elif(object.get_name().contains("wall")):
+			pass
 		
-		# get vectors from center of obj to rectangle corners
-		var corners = []
-		corners.append(Vector2((rect.size[0] * gscale[0])/2, (rect.size[1] * gscale[1])/2))
-		corners.append(Vector2(-(rect.size[0] * gscale[0])/2, (rect.size[1] * gscale[1])/2))
-		corners.append(Vector2(-(rect.size[0] * gscale[0])/2, -(rect.size[1] * gscale[1])/2))
-		corners.append(Vector2((rect.size[0] * gscale[0])/2, -(rect.size[1] * gscale[1])/2))
-		
-		# add to global var to access during updates
-		var collider = [null, null, null, null]
-		ground_verts.append([ground, corners, collider])
+		# any other object, add to excluded	
+		else:
+			# if lantern, assign it to the light source
+			if(object.get_name().contains("Lantern")):
+				lantern = object
+			excluded_obj.append(object)
+			
+	print(excluded_obj)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
@@ -45,94 +59,139 @@ func _physics_process(delta):
 	draw_verts = []
 	test_draw = []
 	test_red = []
+	test_points = []
 	
-	# select which light rays are actually important and make colliders
-	for object in ground_verts:
-		
+	# loop:
+	
+	# loop through each light object
+	for object in shadowcast_obj:
 		var space_state = get_parent().get_world_2d().direct_space_state
-		var ground = object[0]
-		var corners = object[1]
+		var obj_node = object[0]
+		var vertices = object[1]
 		var collider = object[2]
+		# vertex order: ray vertex, ray end, next end.., ray end2, ray vertex2, vertex going back
+		var collider_points = []
+		var collider_polygon = []
 		
-		# make corner to vectors to corners
-		var gpos = ground.position
-		for i in range(len(corners)):
-			var vect = Vector2(corners[i-1][0] + gpos[0], corners[i-1][1] + gpos[1])
-			var edge1 = corners[i%len(corners)] - corners[i-1]
-			var edge2 = corners[(i-2)%len(corners)] - corners[i-1]
+		# find the bad vertex (obtuse bisector)
+		for i in range(len(vertices)):
+			var edge1 = vertices[(i+1)%len(vertices)] - vertices[i]
+			var edge2 = vertices[(i-1)%len(vertices)] - vertices[i]
+			var light_ray = vertices[i] - lantern.position
 			
-			var light_ray = vect - lantern.position
-			var query = PhysicsRayQueryParameters2D.create(lantern.position, vect)
-			query.exclude = ray_exclude
-			var result = space_state.intersect_ray(query)
-			
-			# check if it intersects with anything in front
-			var blocked = true
-			if result == {}:
-				blocked = false
-			elif result != null:
-				if (result.position - vect).length() <= 1:
-					blocked = false
-			
-			# check if the light is colliding or keeps going afterwards
 			var solid = false
 			var betweenv1 = (edge1.normalized() - light_ray.normalized()).normalized()
 			var betweenv2 = (edge2.normalized() - light_ray.normalized()).normalized()
 			var angle = acos(betweenv1.dot(betweenv2))
 
+			
 			# account for floating point error
 			if(angle - 0.001 >= PI/4):
 				solid = true
-				test_red.append([corners[i-1]+gpos, betweenv1*20])
-				test_red.append([corners[i-1]+gpos, betweenv2*20])
+				test_red.append([vertices[i], betweenv1*20])
+				test_red.append([vertices[i], betweenv2*20])
 			else:
-				test_draw.append([corners[i-1]+gpos, betweenv1*20])
-				test_draw.append([corners[i-1]+gpos, betweenv2*20])
-							
-			# check if relevant
-			if blocked == false and solid == false:
-				# if yes, move or make collider
-				draw_verts.append(vect)
+				test_draw.append([vertices[i], betweenv1*20])
+				test_draw.append([vertices[i], betweenv2*20])
 				
-				# physics collision to find end point
-				var query2 = PhysicsRayQueryParameters2D.create(vect + light_ray.normalized()*1, vect + light_ray.normalized()*2000)
-				query2.exclude = ray_exclude
-				var result2 = space_state.intersect_ray(query2)
-				
-				# if it collides...
-				if result2 != null:
-					if result2.size() != 0:
-						var segment = SegmentShape2D.new()
-						segment.a = vect
-						segment.b = result2.position
+		# find raycast end positions of other vertices 
+			if(solid == false):
+				collider_points.append(vertices[i])
+				test_points.append(vertices[i])
+				var query = PhysicsRayQueryParameters2D.create(vertices[i] + light_ray.normalized()*1, vertices[i] + light_ray.normalized()*2000)
+				query.exclude = excluded_obj
+				var result = space_state.intersect_ray(query)
+				if result != null:
+					if result.size() != 0:
+						test_points.append(result.position)
+						collider_points.append(result.position)
 						
-						# check if vectex has a light wall yet
-						if(collider[i-1] == null):
-							# make collider
-							collider[i-1] = light_wall.instantiate()
-							add_child(collider[i-1])
-						# move position
-						collider[i-1].get_node("CollisionShape2D").shape = segment
-					
-			# if no, delete collider if there is one
-			else:
-				if(collider[i-1] != null):
-					collider[i-1].queue_free()
-					collider[i-1] = null
+		# move collider to verticies and end position locations
+		collider_polygon = collider_points
+		collider.get_node("CollisionPolygon2D").set_polygon(collider_polygon)
+#
+#		# make corner to vectors to corners
+#		var gpos = ground.position
+#		for i in range(len(corners)):
+#			var vect = Vector2(corners[i-1][0] + gpos[0], corners[i-1][1] + gpos[1])
+#			var edge1 = corners[i%len(corners)] - corners[i-1]
+#			var edge2 = corners[(i-2)%len(corners)] - corners[i-1]
+#
+#			var light_ray = vect - lantern.position
+#			var query = PhysicsRayQueryParameters2D.create(lantern.position, vect)
+#			query.exclude = ray_exclude
+#			var result = space_state.intersect_ray(query)
+#
+#			# check if it intersects with anything in front
+#			var blocked = true
+#			if result == {}:
+#				blocked = false
+#			elif result != null:
+#				if (result.position - vect).length() <= 1:
+#					blocked = false
+#
+#			# check if the light is colliding or keeps going afterwards
+#			var solid = false
+#			var betweenv1 = (edge1.normalized() - light_ray.normalized()).normalized()
+#			var betweenv2 = (edge2.normalized() - light_ray.normalized()).normalized()
+#			var angle = acos(betweenv1.dot(betweenv2))
+#
+#			# account for floating point error
+#			if(angle - 0.001 >= PI/4):
+#				solid = true
+#				test_red.append([corners[i-1]+gpos, betweenv1*20])
+#				test_red.append([corners[i-1]+gpos, betweenv2*20])
+#			else:
+#				test_draw.append([corners[i-1]+gpos, betweenv1*20])
+#				test_draw.append([corners[i-1]+gpos, betweenv2*20])
+#
+#			# check if relevant
+#			if blocked == false and solid == false:
+#				# if yes, move or make collider
+#				draw_verts.append(vect)
+#
+#				# physics collision to find end point
+#				var query2 = PhysicsRayQueryParameters2D.create(vect + light_ray.normalized()*1, vect + light_ray.normalized()*2000)
+#				query2.exclude = ray_exclude
+#				var result2 = space_state.intersect_ray(query2)
+#
+#				# if it collides...
+#				if result2 != null:
+#					if result2.size() != 0:
+#						var segment = SegmentShape2D.new()
+#						segment.a = vect
+#						segment.b = result2.position
+#
+#						# check if vectex has a light wall yet
+#						if(collider[i-1] == null):
+#							# make collider
+#							collider[i-1] = light_wall.instantiate()
+#							add_child(collider[i-1])
+#						# move position
+#						collider[i-1].get_node("CollisionShape2D").shape = segment
+#
+#			# if no, delete collider if there is one
+#			else:
+#				if(collider[i-1] != null):
+#					collider[i-1].queue_free()
+#					collider[i-1] = null
 	
 	
 	
 	# make and move collisions
 	
-	# loop through all rays
-	# check if the ray is relevant
-	# if it is, draw a raycast from the vertex and extend it past in the same direction as the ray
-	# once end point is found...
-	# check if vertex has a collider
-	# if not, make one and set it to vertex and end point
-	# if already has, then move the end point to new end point
-	# if ray is not relevant, delete collider
+	# setup:
+	# loop through every object and determine if it casts light rays.
+	# if ground, add to list, if border, dont, neither, add to raycast ignore list
+	# if lantern, assign it to the light source
+	# set up database and colliders (interact with just player rn)
 	
+	# loop:
+	# loop through each light object
+	# find the bad vertex (obtuse bisector)
+	# find raycast end positions of other vertices 
+	# move collider to verticies and end position locations
+
 # draw relevant light rays
 func _draw():
 	for vertex in draw_verts:	
@@ -143,6 +202,8 @@ func _draw():
 	for pair in test_red:
 		draw_line(pair[0], pair[1] + pair[0], Color.RED, 1)
 		draw_circle(pair[1] + pair[0], 1, Color.RED)
+	for point in test_points:
+		draw_circle(point, 5, Color.BLANCHED_ALMOND)
 	
 			
 			
